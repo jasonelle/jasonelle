4e109072eadf566680b35695892f059efb1f3133
clsource <camilo@ninjas.cl>
2023-02-21 10:48:14 -0300

    added onesignal config example

---
+++ sources/xcode/App/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Camilo Castro on 21-02-23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
