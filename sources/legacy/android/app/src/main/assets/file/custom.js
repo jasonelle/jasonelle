76fa511e3fab962c5affdcb3491b00d22e0f15bc
Camilo <292738+clsource@users.noreply.github.com>
2024-09-21 15:14:26 -0300

    Aded OneSignal support for Android (#5)
    * android onesignal test
    * added docs

--- sources/legacy/android/app/src/main/assets/file/custom.js
+++ sources/legacy/android/app/src/main/assets/file/custom.js
@@ -4,3 +4,35 @@ Use it to modify the website with javascript.
 */
 
 console.log("file://custom.js loaded");
+
+
+console.log("Initializing OneSignal");
+
+const $onesignal = {};
+
+// OneSignal Helper
+/*
+Trigger actions defined in $jason.head.actions
+
+"head": {
+      "actions": {
+        "onesignal.login": {
+          "type": "$onesignal.login",
+          "options": {
+            "externalid": "{{$jason.data.externalid}}"
+          }
+        },
+        "onesignal.logout": {
+          "type": "$onesignal.logout",
+          "options": {}
+        }
+      }
+    }
+*/
+$onesignal.login = (external_id) => {
+  $agent.trigger("onesignal.login", {externalid: external_id});
+};
+
+$onesignal.logout = () => {
+  $agent.trigger("onesignal.logout");
+};
